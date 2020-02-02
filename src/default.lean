8d48643f70f525b00479366b21c485aaedc1acd3
Markus Himmel <markus@himmel-villmar.de>
2020-02-02 15:09:54 +0100

    Finish proof of the splitting lemma

--- src/default.lean
+++ src/default.lean
@@ -1,35 +1,40 @@
 import linear_algebra.basic
 import linear_algebra.direct_sum_module
-import tactic.ring
 open linear_map
 
 --universes u v w x y z a
 --variables {R : Type u} {A : Type v} {B : Type w} {C : Type x} { P : Type y} { N : Type z } { M : Type a }
 
 section
-variables {R : Type} {A : Type} {B : Type} {C : Type}
-variables [ring R] [add_comm_group A] [add_comm_group B] [add_comm_group C]
-variables [module R A] [module R B] [module R C]
+  variables {R : Type} {A : Type} {B : Type} {C : Type}
+  variables [ring R] [add_comm_group A] [add_comm_group B] [add_comm_group C]
+  variables [module R A] [module R B] [module R C]
 
-lemma comp_vanish {f : A →ₗ[R] B} {g : B →ₗ[R] C} (h : range f ≤ ker g) : comp g f = 0 :=
-by { ext, rw comp_apply, exact (iff.mp mem_ker) (h $ submodule.mem_map_of_mem trivial) }
+  lemma symm_mover {f : A ≃ₗ[R] B} {g : B →ₗ[R] C} {h : A →ₗ[R] C} (h₁ : h = comp g f) : comp h (f.symm : B →ₗ[R] A) = g :=
+  by { ext, rw h₁, simp only [comp_apply, linear_equiv.coe_apply, linear_equiv.apply_symm_apply], }
 
-lemma range_ker {f : A →ₗ[R] B} {g : B →ₗ[R] C} (h : comp g f = 0) : range f ≤ ker g :=
-begin
-  refine le_ker_iff_map.mpr _,
-  ext,
-  simp only [submodule.mem_map, mem_range, submodule.mem_bot],
-  exact ⟨λ ⟨b, ⟨a, h₁⟩, h₂⟩,
-    by { rw [←h₁, ←comp_apply, h, zero_apply] at h₂, exact eq.symm h₂, },
-    λ h₁, ⟨0, ⟨⟨0, by simp⟩, by { rw map_zero, apply eq.symm, exact h₁ }⟩⟩⟩
-end
+  lemma symm_movel {f : B ≃ₗ[R] C} {g : A →ₗ[R] B} {h : A →ₗ[R] C} (h₁ : h = comp (f : B →ₗ[R] C) g) : comp (f.symm : C →ₗ[R] B) h = g :=
+  by { ext, rw h₁, simp only [comp_apply, linear_equiv.coe_apply, linear_equiv.symm_apply_apply], }
 
+  lemma comp_vanish {f : A →ₗ[R] B} {g : B →ₗ[R] C} (h : range f ≤ ker g) : comp g f = 0 :=
+  by { ext, rw comp_apply, exact mem_ker.1 (h $ submodule.mem_map_of_mem trivial) }
+
+  lemma range_ker {f : A →ₗ[R] B} {g : B →ₗ[R] C} (h : comp g f = 0) : range f ≤ ker g :=
+  begin
+    refine le_ker_iff_map.2 _,
+    ext,
+    simp only [submodule.mem_map, mem_range, submodule.mem_bot],
+    exact ⟨λ ⟨b, ⟨a, h₁⟩, h₂⟩,
+      by { rw [←h₁, ←comp_apply, h, zero_apply] at h₂, exact h₂.symm, },
+      λ h₁, ⟨0, ⟨⟨0, by simp⟩, by { rw map_zero, exact h₁.symm }⟩⟩⟩
+  end
 end
 
 section ses
   variables (R : Type) (A : Type) (B : Type) (C : Type)
   variables [ring R] [add_comm_group A] [add_comm_group B] [add_comm_group C]
   variables [module R A] [module R B] [module R C]
+
   structure ses :=
     (i : A →ₗ[R] B)
     (p : B →ₗ[R] C)
@@ -37,17 +42,31 @@ section ses
     (exact : ker p = range i)
     (surj : range p = ⊤)
 
+  structure left_split_ses extends ses R A B C :=
+    (t : B →ₗ[R] A)
+    (splitl : comp t i = (id : A →ₗ[R] A))
+
   structure right_split_ses extends ses R A B C :=
     (s : C →ₗ[R] B)
-    (split : comp p s = (id : C →ₗ C))
+    (splitr : comp p s = (id : C →ₗ[R] C))
 
   structure direct_sum_ses extends ses R A B C :=
     (b : (A × C) ≃ₗ[R] B)
-    (comm_right : comp p (b : (A × C) →ₗ[R] B) = snd R A C)
-    (comm_left : comp (b : (A × C) →ₗ[R] B) (inl R A C) = i)
+    (commr : comp p (b : (A × C) →ₗ[R] B) = snd R A C)
+    (comml : comp (b : (A × C) →ₗ[R] B) (inl R A C) = i)
+
+  structure split_ses extends ses R A B C :=
+    (t : B →ₗ[R] A)
+    (s : C →ₗ[R] B)
+    (b : (A × C) ≃ₗ[R] B)
+    (splitr : comp p s = (id : C →ₗ[R] C))
+    (splitl : comp t i = (id : A →ₗ[R] A))
+    (commr : comp p (b : (A × C) →ₗ[R] B) = snd R A C)
+    (comml : comp (b : (A × C) →ₗ[R] B) (inl R A C) = i)
+
 end ses
 
-section
+section ses
   variables {R : Type} {A : Type} {B : Type} {C : Type}
   variables [ring R] [add_comm_group A] [add_comm_group B] [add_comm_group C]
   variables [module R A] [module R B] [module R C]
@@ -59,28 +78,75 @@ section
   by rw [←comp_apply, ses_vanish, zero_apply]
 
   lemma ses_pull {s : ses R A B C} {b : B} (h : s.p b = 0) : ∃ (a : A), s.i a = b :=
-  mem_range.mp $ by { rw ←s.exact, exact mem_ker.mpr h, }
+  mem_range.1 $ by { rw ←s.exact, exact mem_ker.2 h, }
+
+  @[simp] lemma left_split_apply {s : left_split_ses R A B C} { a : A} : s.t (s.i a) = a :=
+  by rw [←comp_apply, s.splitl, id_apply]
 
   @[simp] lemma right_split_apply {s : right_split_ses R A B C} {c : C} : s.p (s.s c) = c :=
-  by rw [←comp_apply, s.split, id_apply]
+  by rw [←comp_apply, s.splitr, id_apply]
 
   @[simp] lemma direct_sum_apply_right {s : direct_sum_ses R A B C} {a : A} {c : C} : s.p (s.b ⟨a, c⟩) = c :=
-  by rw [←linear_equiv.coe_apply, ←comp_apply, s.comm_right, snd_apply]
-end
+  by rw [←linear_equiv.coe_apply, ←comp_apply, s.commr, snd_apply]
+
+  @[simp] lemma direct_sum_apply_left {s : direct_sum_ses R A B C} {a : A} : s.b (a, 0) = s.i a :=
+  begin
+    have : (a, 0) = (inl R A C) a := by simp,
+    rw [←linear_equiv.coe_apply, this, ←comp_apply, s.comml],
+  end
+end ses
 
 variables {R : Type} {A : Type} {B : Type} {C : Type} {P : Type} {N : Type} {M : Type}
 
 section splitting_lemma
   variables [ring R] [add_comm_group A] [add_comm_group B] [add_comm_group C]
   variables [module R A] [module R B] [module R C]
+  
+  private noncomputable def direct_sum_of_left_split (r : left_split_ses R A B C) : direct_sum_ses R A B C :=
+  let i := r.i, p := r.p, t := r.t, b := linear_equiv.of_bijective (pair t p)
+  begin
+    apply ker_eq_bot'.2,
+    intros b h,
+    have h₁ : t b = 0 ∧ p b = 0, by simpa using prod.mk.inj h,
+    cases ses_pull h₁.2 with a h₂,
+    have h₃ : a = t b, by simpa using (congr_arg t h₂),
+    simpa [h₃, h₁.1] using h₂.symm,
+  end
+  begin
+    rw [range_eq_top, function.surjective],
+    rintro ⟨a, c⟩,
+    cases range_eq_top.1 r.surj c with b h,
+    exact ⟨i a + b - i (t b), by simp [h]⟩
+  end
+  in ⟨r.to_ses, b.symm,
+  begin
+    apply symm_mover,
+    ext,
+    rw [comp_apply, linear_equiv.coe_apply, linear_equiv.of_bijective_apply],
+    simp,
+  end,
+  begin
+    apply symm_movel,
+    ext1,
+    rw [comp_apply, linear_equiv.coe_apply, linear_equiv.of_bijective_apply],
+    simp,
+  end⟩
 
-  noncomputable theorem direct_sum_of_right_split (r : right_split_ses R A B C) : direct_sum_ses R A B C :=
+  private def left_split_of_direct_sum (r : direct_sum_ses R A B C) : left_split_ses R A B C :=
+  ⟨r.to_ses, comp (fst R A C) (r.b.symm), begin
+    ext,
+    rw [comp_apply, comp_apply, ←direct_sum_apply_left,
+      linear_equiv.coe_apply, linear_equiv.symm_apply_apply],
+    simp,
+  end⟩
+
+  private noncomputable def direct_sum_of_right_split (r : right_split_ses R A B C) : direct_sum_ses R A B C :=
   let i := r.i, p := r.p, s := r.s in ⟨r.to_ses, linear_equiv.of_bijective (copair i s)
   begin
-    apply ker_eq_bot'.mpr,
+    apply ker_eq_bot'.2,
     rintros ⟨a, c⟩ h,
-    have c₀ : c = 0, by { convert congr_arg p h; simp },
-    have a₀ : a = 0, by { apply ker_eq_bot'.mp r.inj a, convert h, simp [c₀], },
+    have c₀ : c = 0, by simpa using congr_arg p h,
+    have a₀ : a = 0, by { apply ker_eq_bot'.1 r.inj a, convert h, simp [c₀], },
     simpa [c₀, a₀],
   end
   begin
@@ -93,8 +159,20 @@ section splitting_lemma
   by { ext ⟨a, c⟩, rw [comp_apply, linear_equiv.coe_apply, linear_equiv.of_bijective_apply], simp, },
   by { ext a, rw [comp_apply, linear_equiv.coe_apply, linear_equiv.of_bijective_apply], simp, }⟩
 
-  theorem right_split_of_direct_sum (r : direct_sum_ses R A B C) : right_split_ses R A B C :=
+  private def right_split_of_direct_sum (r : direct_sum_ses R A B C) : right_split_ses R A B C :=
   ⟨r.to_ses, comp (r.b : (A × C) →ₗ[R] B) (inr R A C), by { ext c, simp, }⟩
+
+  def split_of_direct_sum (d : direct_sum_ses R A B C) : split_ses R A B C :=
+  let l := left_split_of_direct_sum d, r := right_split_of_direct_sum d in
+  ⟨d.to_ses, l.t, r.s, d.b, r.splitr, l.splitl, d.commr, d.comml⟩
+
+  noncomputable def split_of_right_split (r : right_split_ses R A B C) : split_ses R A B C :=
+  let d := direct_sum_of_right_split r, l := left_split_of_direct_sum d in
+  ⟨r.to_ses, l.t, r.s, d.b, r.splitr, l.splitl, d.commr, d.comml⟩
+
+  noncomputable def split_of_left_split (l : left_split_ses R A B C) : split_ses R A B C :=
+  let d := direct_sum_of_left_split l, r := right_split_of_direct_sum d in
+  ⟨l.to_ses, l.t, r.s, d.b, r.splitr, l.splitl, d.commr, d.comml⟩
 end splitting_lemma
 
 section projective
